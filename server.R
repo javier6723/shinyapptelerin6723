c2bf29b5a0291a0cce1612359da138898760cba4
javier6723 <javier6723@yahoo.es>
2014-09-21 18:43:17 -0430

    third commit

--- server.R
+++ server.R
@@ -3,7 +3,6 @@ data(state)
 statex77<-as.data.frame(state.x77)
 colnames(statex77)[4] = "Life.Exp"    
 colnames(statex77)[6] = "HS.Grad"
-statex77$Density = statex77$Population * 1000 / statex77$Area
 
 modelfit<-lm(formula = Life.Exp ~ Population + Murder + HS.Grad + Frost,data = statex77)
 
--- ui.R
+++ ui.R
@@ -3,15 +3,17 @@ library(shiny)
 shinyUI(
     pageWithSidebar(
     #application title
-        headerPanel("Prediction of LIfe Expectancy"),
+        headerPanel("Prediction of Life Expectancy"),
         sidebarPanel(
+                     p('This shiny app predict the life expectancy in years in the USA using as regressors in a multiple regression model the population estimate as of July 1 1975, murder and non-negligent manslaughter rate per 100,000 population (1976), percent high-school graduates (1970). The data came from  the state.x77  part of "state" R datasets.'),                
+                     p('Please insert the values to predit the Life expentacy'),
                      numericInput('inPopulation', 'Population', 0),
                      numericInput('inMurder', 'Murder', 0),
                      numericInput('inHS.Grad', 'HS.Grad', 0),
                      numericInput('inFrost', 'Frost', 0),
                      submitButton('Submit')
                      ), 
-        mainPanel(
+        mainPanel(       
             h3('Summary of the model'),
             verbatimTextOutput("summary"),
             h3("Prediction"),
